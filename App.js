ecf1b232a3249f11d8c44d10ea6c232cecc46c37
Dylan Widjaja <dwidjaja945@gmail.com>
2018-07-31 17:44:21 -0700

    can now push a button and get coordinates of location

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import FetchLocation from './components/FetchLocation';
+
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>It works!</Text>
-      </View>
-    );
-  }
+	getUserLocationHandler = () => {
+		navigator.geolocation.getCurrentPosition((position) => {
+			console.log(position);
+		},
+		err => console.log(err));
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<FetchLocation onGetLocation={this.getUserLocationHandler} />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
